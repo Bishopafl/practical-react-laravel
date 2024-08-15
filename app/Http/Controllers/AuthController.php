5af9f0a1b4bb4ced9df5de496cba98d4798c8116
Adam Lopez <adamf04@gmail.com>
2024-08-15 14:44:32 -0400

    Adding login logic and throwing an unauthorized response to the user if the credentials are not correct

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,14 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {
-    public function register(Request $request) {
+    public function register(RegisterRequest $request)
+    {
         $user = User::create([
             'first_name' => $request->input('first_name'),
             'last_name' => $request->input('last_name'),
@@ -18,5 +21,25 @@ class AuthController extends Controller
         ]);
 
         return response($user, Response::HTTP_CREATED);
+    }
+
+    public function login (Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return \response([
+                'error' => 'Invalid Credentials',
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        /**
+         * @var User $user
+         */
+        $user = Auth::user();
+
+        $token = $user->createToken('token')->plainTextToken;
+
+        return \response([
+            'jwt' => $token
+        ]);
     }
 }
