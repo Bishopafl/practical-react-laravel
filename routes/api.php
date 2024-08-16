f5dfb6377e99223f58b2934000b733d08a967f3b
Adam Lopez <adamf04@gmail.com>
2024-08-16 10:56:52 -0400

    Adding logout route to the api

--- routes/api.php
+++ routes/api.php
@@ -25,5 +25,6 @@ Route::post('login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function() {
     Route::get('user', [AuthController::class, 'user']);
+    Route::post('logout', [AuthController::class, 'logout']);
 });
 
