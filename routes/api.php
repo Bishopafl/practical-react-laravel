6df5af1456c9c2c9274702e4d090e0606456eb91
Adam Lopez <adamf04@gmail.com>
2024-08-23 00:17:37 -0400

    deleted un-used routes and adding two more to update user information

--- routes/api.php
+++ routes/api.php
@@ -27,12 +27,8 @@ Route::post('login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function() {
     Route::get('user', [AuthController::class, 'user']);
     Route::post('logout', [AuthController::class, 'logout']);
-
-    // Route::get('users', [UserController::class, 'index'])->name('get_all_users');
-    // Route::post('users', [UserController::class, 'store'])->name('store_user');
-    // Route::get('users/{id}', [UserController::class, 'show'])->name('show_user');
-    // Route::put('users/{id}', [UserController::class, 'update'])->name('update_user');
-    // Route::delete('users/{id}', [UserController::class, 'destroy'])->name('delete_user');
+    Route::put('users/info', [AuthController::class, 'updateInfo']);
+    Route::put('users/password', [AuthController::class, 'updatePassword']);
 
     Route::apiResource('users', UserController::class);
 });
